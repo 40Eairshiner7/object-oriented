1d28f5165d70c02648350cd0ba381670c313231c
40Eairshiner7 <609182880@qq.com>
2016-03-26 21:36:47 +0800

    Update Scan.cpp

--- Calculator/Scan.cpp
+++ Calculator/Scan.cpp
@@ -17,13 +17,16 @@ using namespace std;
 queue<string> Scan::ToStringQueue(string input){
     
     tooBig = false;
+    //计算数字位数的字符串
     int count=0;
+    //用于组合数字或符号的字符串  
     string item = "";
     std::queue<string> charQueue;
     
     for (int i=0; i<input.length(); i++)
     {
         
+        //是符号则先将组合好的数字压入队列然后才压入符号     
         if(input[i]=='+' || input[i]=='-'|| input[i]=='*' || input[i]=='/' || input[i]=='(' || input[i]==')' )
         {
             if(!item.empty()){
@@ -37,6 +40,7 @@ queue<string> Scan::ToStringQueue(string input){
             item.clear();
         }
         
+        //组合数字的过程里计算位数，注意：小数点不要计入位数中！
         else if((input[i] >= 48 && input[i] <=57) || input[i] == '.' )
         {
             item += input[i];
@@ -50,6 +54,7 @@ queue<string> Scan::ToStringQueue(string input){
         }
     }
     
+    //如果最后一个字符是数字则要记住将其压入
     if(!item.empty())
     {
         charQueue.push(item);
