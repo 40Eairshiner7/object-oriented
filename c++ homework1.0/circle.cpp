20f891004d4b711bb833932d61e44730d01da519
airshiner <609182880@qq.com>
2016-05-05 23:43:40 +0800

    c++ homework1.0

---
+++ c++ homework1.0/circle.cpp
@@ -0,0 +1,17 @@
+//
+//  circle.cpp
+//  c++ homework1.0
+//
+//  Created by airshiner on 5/5/16.
+//  Copyright © 2016 airshiner. All rights reserved.
+//
+
+#include "circle.hpp"
+
+void circle(){
+    
+    double r;
+    cin >> r;
+    cout << r * r * Pi;
+    
+}
